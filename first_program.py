7d64181c031013cf3427ae69e2e869c4f68e5956
Yash Jain <69360295+yashjain1974@users.noreply.github.com>
2022-08-05 13:36:10 +0530

    Added first program

---
+++ first_program.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+print("Hello World")
+a=int(input("Enter number 1:"))
+b=int(input("Enter number 2:"))
+print(a+b)
